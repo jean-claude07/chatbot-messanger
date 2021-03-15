c80cbbf0d17069b0662ec6d522fba3d797e1a1eb
jean-claude07 <jeclaude78@gmail.com>
2021-03-16 01:30:13 +0300

    params

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,8 +2,10 @@ require 'facebook/messenger'
 
 include Facebook::Messenger
 
+SUBSCRIBED_PARAMS = [ 'messages', 'message_echoes', 'message_deliveries', 'messaging_optins', 'leadgen' ]
+
 Facebook::Messenger::Subscriptions.subscribe(
-  access_token: ENV["ACCESS_TOKEN"], subscribed_fields: ['messages', 'message_deliveries']
+  access_token: ENV["ACCESS_TOKEN"], subscribed_fields: SUBSCRIBED_PARAMS
 )
 
 Bot.on :message do |message| 
