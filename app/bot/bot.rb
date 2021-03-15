ceca61fee87f68407a243da4dab12764548db4c9
jean-claude07 <jeclaude78@gmail.com>
2021-03-16 01:33:55 +0300

    git

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,7 +2,7 @@ require 'facebook/messenger'
 
 include Facebook::Messenger
 
-SUBSCRIBED_PARAMS = [ 'messages', 'message_echoes', 'message_deliveries', 'messaging_optins', 'leadgen' ]
+SUBSCRIBED_PARAMS = [ 'messages']
 
 Facebook::Messenger::Subscriptions.subscribe(
   access_token: ENV["ACCESS_TOKEN"], subscribed_fields: SUBSCRIBED_PARAMS
